d2f882ad5ce02a66d7eec7c97ab853fb86fd41e0
re-eun <pajeni@naver.com>
2024-02-27 18:09:14 +0900

    Docs : 템플릿 적용
    -이슈, PR 템플릿 적용

---
+++ src/main/kotlin/org/spartaa3/movietogather/MovieToGatherApplication.kt
@@ -0,0 +1,11 @@
+package org.spartaa3.movietogather
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MovieToGatherApplication
+
+fun main(args: Array<String>) {
+    runApplication<MovieToGatherApplication>(*args)
+}
---
+++ src/test/kotlin/org/spartaa3/movietogather/MovieToGatherApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.spartaa3.movietogather
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MovieToGatherApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
