341f92df2125842c996567ab9e0df7d3403a28c4
재영이 <jeayeong70@gmail.com>
2024-02-29 12:38:00 +0900

    Test : "ReviewService 테스트 코드 작성 마무리 & ReviewControllerTest 작성 시작" ReviewServiceImpl 중 update에 트랜잭션이 빠져있어 추가했습니다.

--- src/main/kotlin/org/spartaa3/movietogather/domain/review/service/ReviewServiceImpl.kt
+++ src/main/kotlin/org/spartaa3/movietogather/domain/review/service/ReviewServiceImpl.kt
@@ -24,6 +24,7 @@ class ReviewServiceImpl(
 
     @Transactional
     override fun createReview(request: CreateReviewRequest): ReviewResponse {
+
         return reviewRepository.save<Review?>(
             Review(
                 postingTitle = request.postingTitle,
@@ -34,12 +35,13 @@ class ReviewServiceImpl(
                 genre = request.genre
             )
         ).toResponse()
+
     }
 
+    @Transactional
     override fun updateReview(reviewId: Long, request: UpdateReviewRequest): ReviewResponse {
         val review = reviewRepository.findByIdOrNull(reviewId) ?: throw ReviewNotFoundException("Review", reviewId)
         val (postingTitle, star, movieTitle, movieImg, contents, genre) = request
-
         review.postingTitle = postingTitle
         review.star = star
         review.movieTitle = movieTitle
--- src/test/kotlin/org/spartaa3/movietogather/domain/review/controller/ReviewControllerTest.kt
+++ src/test/kotlin/org/spartaa3/movietogather/domain/review/controller/ReviewControllerTest.kt
@@ -1,8 +1,55 @@
 package org.spartaa3.movietogather.domain.review.controller
 
-import io.kotest.core.spec.style.FunSpec
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.clearAllMocks
+import io.mockk.every
+import io.mockk.junit5.MockKExtension
+import io.mockk.mockk
+import org.junit.jupiter.api.extension.ExtendWith
+import org.spartaa3.movietogather.domain.review.dto.ReviewResponse
+import org.spartaa3.movietogather.domain.review.service.ReviewService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.MediaType
 
-class ReviewControllerTest : FunSpec({
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import java.time.LocalDateTime
 
+@WebMvcTest
+@AutoConfigureMockMvc
+@ExtendWith(MockKExtension::class)
+class ReviewControllerTest @Autowired constructor(
+    private val mockMvc: MockMvc,
+) : DescribeSpec({
 
+    afterContainer {
+        clearAllMocks()
+    }
+    val reviewService = mockk<ReviewService>()
+    describe("GET /api/reviews/{reviewId} 는") {
+        val reviewId = 1L
+        every { reviewService.getReviewById(any()) } returns ReviewResponse(
+            id = reviewId,
+            postingTitle = "postingTitle",
+            star = 5.0,
+            movieTitle = "movieTitle",
+            movieImg = "movieImg",
+            contents = "contents",
+            genre = "genre",
+            createdAt = LocalDateTime.now()
+        )
+        context("존재하는 Id에 대한 요청을 보낼 때") {
+            val result= mockMvc.perform(
+                MockMvcRequestBuilders.get("/api/reviews/$reviewId")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON)
+            ).andReturn()
+            it("200 status code를 응답해야 한다.") {
+                result.response.status shouldBe 200
+            }
+        }
+    }
 })
