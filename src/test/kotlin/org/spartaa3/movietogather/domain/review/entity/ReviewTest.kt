6ac3255015e14b6d2be89243febd64ba629a370f
re-eun <pajeni@naver.com>
2024-03-04 20:16:22 +0900

    Refactor: workflow 수정
    -test

--- src/test/kotlin/org/spartaa3/movietogather/domain/review/entity/ReviewTest.kt
+++ src/test/kotlin/org/spartaa3/movietogather/domain/review/entity/ReviewTest.kt
@@ -1,33 +1,33 @@
-//package org.spartaa3.movietogather.domain.review.entity
-//
-//import io.kotest.core.spec.style.BehaviorSpec
-//import io.kotest.matchers.shouldBe
-//import java.time.LocalDateTime
-//
-//class ReviewTest : BehaviorSpec({
-//    given("Review Entity가 주어졌을 때") {
-//        val review = Review(
-//            postingTitle = "postingTitle",
-//            movieTitle = "movieTitle",
-//            movieImg = "movieImg",
-//            genre = "genre",
-//            contents = "contents",
-//            star = 5.0,
-//            createdAt = LocalDateTime.now()
-//        )
-//
-//        `when`("Entity를 Response로 변환하면") {
-//            val response = review.toResponse()
-//            then("Entity에 정의된 값과 반환된 Response의 값은 같아야 한다.") {
-//                response.postingTitle shouldBe review.postingTitle
-//                response.genre shouldBe review.genre
-//                response.star shouldBe review.star
-//                response.movieTitle shouldBe review.movieTitle
-//                response.movieImg shouldBe review.movieImg
-//                response.contents shouldBe review.contents
-//                response.createdAt shouldBe review.createdAt
-//
-//            }
-//        }
-//    }
-//})
+package org.spartaa3.movietogather.domain.review.entity
+
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import java.time.LocalDateTime
+
+class ReviewTest : BehaviorSpec({
+    given("Review Entity가 주어졌을 때") {
+        val review = Review(
+            postingTitle = "postingTitle",
+            movieTitle = "movieTitle",
+            movieImg = "movieImg",
+            genre = "genre",
+            contents = "contents",
+            star = 5.0,
+            createdAt = LocalDateTime.now()
+        )
+
+        `when`("Entity를 Response로 변환하면") {
+            val response = review.toResponse()
+            then("Entity에 정의된 값과 반환된 Response의 값은 같아야 한다.") {
+                response.postingTitle shouldBe review.postingTitle
+                response.genre shouldBe review.genre
+                response.star shouldBe review.star
+                response.movieTitle shouldBe review.movieTitle
+                response.movieImg shouldBe review.movieImg
+                response.contents shouldBe review.contents
+                response.createdAt shouldBe review.createdAt
+
+            }
+        }
+    }
+})
