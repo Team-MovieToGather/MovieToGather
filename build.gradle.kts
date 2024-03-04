57e8aeca4effb42418df014cd3e6e3660f7e10af
re-eun <pajeni@naver.com>
2024-03-04 17:33:18 +0900

    Feat : Github Actions CI 기능 추가
    -workflow 디렉토리 생성
    -CI yml 파일 생성, 작성
    -그 외 : implementation("io.jsonwebtoken:jjwt-api:0.12.3") 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,20 +50,26 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-aop")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
     implementation("org.springframework.boot:spring-boot-starter-cache")
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     implementation("com.querydsl:querydsl-jpa:$queryDslVersion:jakarta")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    implementation ("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation ("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("io.jsonwebtoken:jjwt-api:0.12.3")
+    implementation("io.jsonwebtoken:jjwt-impl:0.12.3")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.12.3")
+
+
     kapt("com.querydsl:querydsl-apt:$queryDslVersion:jakarta")
     kapt("jakarta.annotation:jakarta.annotation-api")
     kapt("jakarta.persistence:jakarta.persistence-api")
-    compileOnly ("org.projectlombok:lombok:1.18.20")
-    annotationProcessor ("org.projectlombok:lombok:1.18.20")
-    implementation ("org.springframework.boot:spring-boot-starter-data-redis")
 
-    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.3")
-    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.3")
+    compileOnly ("org.projectlombok:lombok:1.18.20")
+
+    annotationProcessor ("org.projectlombok:lombok:1.18.20")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
     runtimeOnly("org.postgresql:postgresql")
-    implementation ("org.springframework.boot:spring-boot-starter-thymeleaf")
+
 
     // 테스트 코드
     testRuntimeOnly("com.h2database:h2")
