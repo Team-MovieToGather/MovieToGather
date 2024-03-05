5ddde83fe434119a2e4305d5c504d91f87ad08b1
re-eun <pajeni@naver.com>
2024-03-05 09:41:28 +0900

    Refactor: 테스트파일 수정
    -빌드 실패 원인 파악용
    -@ActiveProfiles 어노테이션

--- src/test/kotlin/org/spartaa3/movietogather/domain/review/MovieToGatherApplicationTests.kt
+++ src/test/kotlin/org/spartaa3/movietogather/domain/review/MovieToGatherApplicationTests.kt
@@ -2,7 +2,9 @@ package org.spartaa3.movietogather
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
+@ActiveProfiles("test")
 @SpringBootTest
 class MovieToGatherApplicationTests {
 
