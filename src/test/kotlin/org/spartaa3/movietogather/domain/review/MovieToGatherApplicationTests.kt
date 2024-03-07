8fa3bd188434e87076939177c2ac94ca3d6795ab
re-eun <pajeni@naver.com>
2024-03-07 09:46:47 +0900

    Refactor: workflow 수정
    -.env 파일 생성
    -workflow .env 파일 생성 로직
    -@SpringBootTest 클래스 명시 제거
    -ReviewServiceImplTest 주석처리

--- src/test/kotlin/org/spartaa3/movietogather/domain/review/MovieToGatherApplicationTests.kt
+++ src/test/kotlin/org/spartaa3/movietogather/domain/review/MovieToGatherApplicationTests.kt
@@ -3,7 +3,7 @@ package org.spartaa3.movietogather.domain.review
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest(classes = [MovieToGatherApplicationTests::class])
+@SpringBootTest
 class MovieToGatherApplicationTests {
 
     @Test
