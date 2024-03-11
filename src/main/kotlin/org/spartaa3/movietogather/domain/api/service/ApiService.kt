95434dd79a7cd80f171ab365a0e77c5315677fb3
re-eun <pajeni@naver.com>
2024-03-11 14:14:17 +0900

    Refactor : 파라미터명 수정

--- src/main/kotlin/org/spartaa3/movietogather/domain/api/service/ApiService.kt
+++ src/main/kotlin/org/spartaa3/movietogather/domain/api/service/ApiService.kt
@@ -4,5 +4,5 @@ import org.spartaa3.movietogather.domain.api.service.dto.response.MovieResponse
 import org.springframework.data.domain.SliceImpl
 
 interface ApiService {
-    fun getMovies(title: String?, page: Int): SliceImpl<MovieResponse>
+    fun getMovies(title: String?, pageNumber: Int): SliceImpl<MovieResponse>
 }
