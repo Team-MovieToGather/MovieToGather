683849b3f846b645c75c4d33aec97a28f87c441a
재영이 <jeayeong70@gmail.com>
2024-03-14 11:40:01 +0900

    Feat : "api 교체 로직 추가&주소 출력 자료형 List로 변경"

--- src/main/kotlin/org/spartaa3/movietogather/infra/map/service/KakaoMapService.kt
+++ src/main/kotlin/org/spartaa3/movietogather/infra/map/service/KakaoMapService.kt
@@ -1,9 +1,9 @@
-package org.spartaa3.movietogather.domain.map.service
+package org.spartaa3.movietogather.infra.map.service
 
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.github.cdimascio.dotenv.Dotenv
-import org.spartaa3.movietogather.domain.map.dto.KakaoMapResponse
+import org.spartaa3.movietogather.infra.map.dto.KakaoMapResponse
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestClient
@@ -15,19 +15,25 @@ class KakaoMapService {
         jacksonObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
 
     fun searchCoordinate(keyword: String): Map<String, String> {
+        val result = searchAddress(keyword)
+        //TODO(프론트에서 클릭한 데이터를 기반으로 사용해야 하기 때문에 후에 수정이 필요)
+        val x = result[0].x
+        val y = result[0].y
+        val coordinate = mapOf("x" to x, "y" to y)
+        return coordinate
+    }
+
+    fun searchAddress(keyword: String): List<KakaoMapResponse.Documents> {
         val dotenv = Dotenv.load()["KAKAO_MAP_AK"]
         val result = restClient.get()
             .uri("https://dapi.kakao.com/v2/local/search/address.json?query=${keyword}")
             .accept(MediaType.APPLICATION_JSON)
             .header("Authorization", "KakaoAK $dotenv")
             .retrieve()
-            .body(String::class.java)
+            .body(String::class.java) ?: throw Exception("No result")
         val json = objectMapper.readValue(result, KakaoMapResponse::class.java)
-        //TODO(프론트에서 클릭한 데이터를 기반으로 사용해야 하기 때문에 후에 수정이 필요)
-        val x = json.documents[0].x
-        val y = json.documents[0].y
-        val coordinate = mapOf("x" to x, "y" to y)
-        return coordinate
+        val addressList = json.documents
+        return addressList
     }
 
 }
