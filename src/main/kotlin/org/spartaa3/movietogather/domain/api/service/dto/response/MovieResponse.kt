a5e0f112b41e7c4e9f40208703edf9375de0d131
re-eun <pajeni@naver.com>
2024-03-15 16:44:38 +0900

    Bug : MissingKotlinParameterException 에러
    - poster_path nullable 변경
    - default posterUrl 추가

--- src/main/kotlin/org/spartaa3/movietogather/domain/api/service/dto/response/MovieResponse.kt
+++ src/main/kotlin/org/spartaa3/movietogather/domain/api/service/dto/response/MovieResponse.kt
@@ -4,10 +4,14 @@ data class MovieResponse(
     val id: Int,
     val title: String,
     val genre_ids: List<Int>,
-    val poster_path: String,
+    val poster_path: String?,
     var genreNames: String = ""
 ) {
-    val posterUrl = "https://image.tmdb.org/t/p/w500$poster_path"
+    val posterUrl = if (poster_path != null) {
+        "https://image.tmdb.org/t/p/w500$poster_path"
+    } else {
+        "https://drive.google.com/file/d/1uQKNyT4pyQZaAyhU-xbSS-Qi8jueYkfK/view?usp=sharing"
+    }
 }
 
 data class MovieListResponse(
