38b9863467816a4d091dc181144fc2cd92b8e952
re-eun <pajeni@naver.com>
2024-03-16 22:03:29 +0900

    Refactor : feat/cicd test

--- src/main/kotlin/org/spartaa3/movietogather/infra/map/service/KakaoMapService.kt
+++ src/main/kotlin/org/spartaa3/movietogather/infra/map/service/KakaoMapService.kt
@@ -1,39 +1,39 @@
-//package org.spartaa3.movietogather.infra.map.service
-//
-//import com.fasterxml.jackson.databind.DeserializationFeature
-//import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-//import io.github.cdimascio.dotenv.Dotenv
-//import org.spartaa3.movietogather.infra.map.dto.KakaoMapResponse
-//import org.springframework.http.MediaType
-//import org.springframework.stereotype.Service
-//import org.springframework.web.client.RestClient
-//
-//@Service
-//class KakaoMapService {
-//    private val restClient: RestClient = RestClient.create()
-//    val objectMapper =
-//        jacksonObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-//
-//    fun searchCoordinate(keyword: String): Map<String, String> {
-//        val result = searchAddress(keyword)
-//        //TODO(프론트에서 클릭한 데이터를 기반으로 사용해야 하기 때문에 후에 수정이 필요)
-//        val x = result[0].x
-//        val y = result[0].y
-//        val coordinate = mapOf("x" to x, "y" to y)
-//        return coordinate
-//    }
-//
-//    fun searchAddress(keyword: String): List<KakaoMapResponse.Documents> {
-//        val dotenv = Dotenv.load()["KAKAO_MAP_AK"]
-//        val result = restClient.get()
-//            .uri("https://dapi.kakao.com/v2/local/search/address.json?query=${keyword}")
-//            .accept(MediaType.APPLICATION_JSON)
-//            .header("Authorization", "KakaoAK $dotenv")
-//            .retrieve()
-//            .body(String::class.java) ?: throw Exception("No result")
-//        val json = objectMapper.readValue(result, KakaoMapResponse::class.java)
-//        val addressList = json.documents
-//        return addressList
-//    }
-//
-//}
+package org.spartaa3.movietogather.infra.map.service
+
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import io.github.cdimascio.dotenv.Dotenv
+import org.spartaa3.movietogather.infra.map.dto.KakaoMapResponse
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Service
+import org.springframework.web.client.RestClient
+
+@Service
+class KakaoMapService {
+    private val restClient: RestClient = RestClient.create()
+    val objectMapper =
+        jacksonObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+
+    fun searchCoordinate(keyword: String): Map<String, String> {
+        val result = searchAddress(keyword)
+        //TODO(프론트에서 클릭한 데이터를 기반으로 사용해야 하기 때문에 후에 수정이 필요)
+        val x = result[0].x
+        val y = result[0].y
+        val coordinate = mapOf("x" to x, "y" to y)
+        return coordinate
+    }
+
+    fun searchAddress(keyword: String): List<KakaoMapResponse.Documents> {
+        val dotenv = Dotenv.load()["KAKAO_MAP_AK"]
+        val result = restClient.get()
+            .uri("https://dapi.kakao.com/v2/local/search/address.json?query=${keyword}")
+            .accept(MediaType.APPLICATION_JSON)
+            .header("Authorization", "KakaoAK $dotenv")
+            .retrieve()
+            .body(String::class.java) ?: throw Exception("No result")
+        val json = objectMapper.readValue(result, KakaoMapResponse::class.java)
+        val addressList = json.documents
+        return addressList
+    }
+
+}
--- src/main/kotlin/org/spartaa3/movietogather/infra/map/service/TMapService.kt
+++ src/main/kotlin/org/spartaa3/movietogather/infra/map/service/TMapService.kt
@@ -1,38 +1,38 @@
-//package org.spartaa3.movietogather.infra.map.service
-//
-//import com.fasterxml.jackson.databind.DeserializationFeature
-//import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-//import io.github.cdimascio.dotenv.Dotenv
-//import org.spartaa3.movietogather.infra.map.dto.TMapResponse
-//import org.springframework.http.MediaType
-//import org.springframework.stereotype.Service
-//import org.springframework.web.client.RestClient
-//
-//@Service
-//class TMapService {
-//    private val restClient: RestClient = RestClient.create()
-//    val objectMapper =
-//        jacksonObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-//
-//    fun searchCoordinate(keyword: String): Map<String, String> {
-//        val result = searchAddress(keyword)
-//        //TODO(프론트에서 클릭한 데이터를 기반으로 사용해야 하기 때문에 후에 수정이 필요)
-//        val x = result[0].frontLon
-//        val y = result[0].frontLat
-//        val coordinate = mapOf("x" to x, "y" to y)
-//        return coordinate
-//    }
-//
-//    fun searchAddress(keyword: String): List<TMapResponse.SearchPoiInfo.Pois.Poi> {
-//        val dotenv = Dotenv.load()["TMAP_AK"]
-//        val result = restClient.get()
-//            .uri("https://apis.openapi.sk.com/tmap/pois?version=1&searchKeyword=${keyword}&format=json&callback=result")
-//            .accept(MediaType.APPLICATION_JSON)
-//            .header("appKey", dotenv)
-//            .retrieve()
-//            .body(String::class.java) ?: throw Exception("No result")
-//        val json = objectMapper.readValue(result, TMapResponse::class.java)
-//        val addressList = json.searchPoiInfo.pois.poi
-//        return addressList
-//    }
-//}
+package org.spartaa3.movietogather.infra.map.service
+
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import io.github.cdimascio.dotenv.Dotenv
+import org.spartaa3.movietogather.infra.map.dto.TMapResponse
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Service
+import org.springframework.web.client.RestClient
+
+@Service
+class TMapService {
+    private val restClient: RestClient = RestClient.create()
+    val objectMapper =
+        jacksonObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+
+    fun searchCoordinate(keyword: String): Map<String, String> {
+        val result = searchAddress(keyword)
+        //TODO(프론트에서 클릭한 데이터를 기반으로 사용해야 하기 때문에 후에 수정이 필요)
+        val x = result[0].frontLon
+        val y = result[0].frontLat
+        val coordinate = mapOf("x" to x, "y" to y)
+        return coordinate
+    }
+
+    fun searchAddress(keyword: String): List<TMapResponse.SearchPoiInfo.Pois.Poi> {
+        val dotenv = Dotenv.load()["TMAP_AK"]
+        val result = restClient.get()
+            .uri("https://apis.openapi.sk.com/tmap/pois?version=1&searchKeyword=${keyword}&format=json&callback=result")
+            .accept(MediaType.APPLICATION_JSON)
+            .header("appKey", dotenv)
+            .retrieve()
+            .body(String::class.java) ?: throw Exception("No result")
+        val json = objectMapper.readValue(result, TMapResponse::class.java)
+        val addressList = json.searchPoiInfo.pois.poi
+        return addressList
+    }
+}
