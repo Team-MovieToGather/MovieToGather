0a2a2012b90f353502eff2d87b653e85784f2591
re-eun <pajeni@naver.com>
2024-03-16 21:39:48 +0900

    Refactor : feat/cicd test

---
+++ src/main/kotlin/org/spartaa3/movietogather/infra/DotenvConfig.kt
@@ -0,0 +1,18 @@
+package org.spartaa3.movietogather.infra
+
+import io.github.cdimascio.dotenv.Dotenv
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class DotenvConfig {
+
+    @Bean
+    fun dotenv(): Dotenv {
+        return Dotenv.configure()
+            .directory("/src/main/resources/.env")
+            .ignoreIfMalformed()
+            .ignoreIfMissing()
+            .load();
+    }
+}
