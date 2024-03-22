15596b64f744466661792929a52676fd0dfc30ad
re-eun <pajeni@naver.com>
2024-03-22 18:01:11 +0900

    Refactor : 영화 response 수정
    - pageNumber 1 고정
    - CustomPageResponse 변경
    - review request 필드 수정
    - 의존성 정리

--- src/main/kotlin/org/spartaa3/movietogather/domain/review/dto/ReviewRequest.kt
+++ src/main/kotlin/org/spartaa3/movietogather/domain/review/dto/ReviewRequest.kt
@@ -3,12 +3,12 @@ package org.spartaa3.movietogather.domain.review.dto
 import com.fasterxml.jackson.annotation.JsonCreator
 
 data class CreateReviewRequest @JsonCreator(mode = JsonCreator.Mode.PROPERTIES) constructor(
+    val movieTitle: String, // 선택된 영화 처리 작업 후 수정 예정
+    val movieImg: String,
+    val genre: String,
     val postingTitle: String,
     val star: Double,
-//    val movieTitle: String, // 선택된 영화 처리 작업 후 수정 예정
-//    val movieImg: String,
     val contents: String,
-//    val genre: String
 )
 
 data class UpdateReviewRequest @JsonCreator(mode = JsonCreator.Mode.PROPERTIES) constructor(
