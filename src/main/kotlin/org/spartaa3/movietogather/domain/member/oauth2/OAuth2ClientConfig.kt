af4985c4abaa90431f1c29816e3e5606d01a4e87
re-eun <pajeni@naver.com>
2024-03-28 15:04:31 +0900

    Feat : kakao Config 파일 추가

---
+++ src/main/kotlin/org/spartaa3/movietogather/domain/member/oauth2/OAuth2ClientConfig.kt
@@ -0,0 +1,25 @@
+package org.spartaa3.movietogather.domain.member.oauth2
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.oauth2.client.registration.ClientRegistration
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository
+
+@Configuration
+class OAuth2ClientConfig {
+
+    @Bean
+    fun clientRegistrationRepository(): InMemoryClientRegistrationRepository {
+        val kakao: ClientRegistration = ClientRegistration.withRegistrationId("kakao")
+            .clientId("\${AUTH_KAKAO_ID}")
+            .clientSecret("\${AUTH_KAKAO_SECRET}")
+            .authorizationUri("https://kauth.kakao.com/oauth/authorize")
+            .tokenUri("https://kauth.kakao.com/oauth/token")
+            .userInfoUri("https://kapi.kakao.com/v2/user/me")
+            .redirectUri("http://movie2gather.net/oauth-redirect-kakao")
+            .clientName("Kakao")
+            .build()
+
+        return InMemoryClientRegistrationRepository(kakao)
+    }
+}
