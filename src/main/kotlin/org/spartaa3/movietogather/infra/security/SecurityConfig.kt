7b2dbd65f5051f4dc1742dddb0b36458ef8a0700
re-eun <pajeni@naver.com>
2024-03-28 15:40:34 +0900

    Refactor : 도메인 오타 수정

--- src/main/kotlin/org/spartaa3/movietogather/infra/security/SecurityConfig.kt
+++ src/main/kotlin/org/spartaa3/movietogather/infra/security/SecurityConfig.kt
@@ -53,7 +53,7 @@ class SecurityConfig(
     fun corsConfigurationSource(): CorsConfigurationSource {
         val configuration = CorsConfiguration().apply {
             addAllowedOrigin("http://localhost:3000")
-            addAllowedOrigin("http://movie2gather.net/")
+            addAllowedOrigin("http://movie2gather.net")
             addAllowedMethod("GET")
             addAllowedMethod("POST")
             addAllowedMethod("PUT")
